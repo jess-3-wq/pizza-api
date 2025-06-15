923df0719168418e0ce43154a8f822e7aa9d532f
Jesse <jesse.yegon@student.moringaschool.com>
2025-06-15 20:53:47 +0300

    restaurant

---
+++ server/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrte import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('server.config.Config')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from server.controllers.restaurant_controller import restaurant_bp
+    from server.controllers.pizza_controller import pizza_bp
+    from server.controllers.restaurant_pizza_controller import restaurant_pizza_bp
+
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restaurant_pizza_bp)
+
+    return app
